be402915ff5083f2b60f921d3f75ab532aa6c942
siva <nssp.siva@gmail.com>
2023-10-10 12:46:59 +0530

    ..

--- Task2.py
+++ Task2.py
@@ -1,24 +1,17 @@
 import csv
 
-def read_csv_file(file_path):
-    try:
-        with open(file_path, 'r') as csvfile:
-            csv_reader = csv.DictReader(csvfile)
-            
-            # Read and print the header
-            header = csv_reader.fieldnames
-            print("Header:", header)
-            
-            # Read and print each row of data
-            for row in csv_reader:
-                print("Row:", row)
-    except FileNotFoundError:
-        print("File not found")
-    except Exception as e:
-        print("An error occurred")
+if __name__ == '__main__':
+    file_path=input("Enter file path")
+    mode='r'
+    with open(file=file_path,mode=mode) as myfile:
+        csv_reder=csv.reader(myfile)
+        departments=list(csv_reder)
+        for line in departments:
+            for word in line:
+                print(word,'/t',end=' ')
+                print()
 
-if __name__ == "__main__":
-    file_path = input("Enter the path to the CSV file:")
-    read_csv_file(file_path)
+
+
 
 
